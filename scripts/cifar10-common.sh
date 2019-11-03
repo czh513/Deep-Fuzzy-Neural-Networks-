cefa817722fcd42c65672e8e875c7e3a39f0d35f
Minh Le <minhle.r7@gmail.com>
2019-11-04 00:57:31 +0100

    try half-strength elliptical,updated reg. strength

--- scripts/cifar10-common.sh
+++ scripts/cifar10-common.sh
@@ -1,6 +1,6 @@
 
-relog_conf="--use_relog=True --log_strength_start=-0.8 --log_strength_inc=0.0001 --log_strength_stop=0.8"
+relog_conf="--use_relog=True --log_strength_start=-0.8 --log_strength_inc=0.0001 --log_strength_stop=1"
 elliptical_conf="--use_elliptical=True --curvature_multiplier_start=-0.4 --curvature_multiplier_stop=0.5"
 maxout_conf="--use_maxout=max --max_folding_factor=4"
-maxfit_conf="--regularization=max-fit --l1=0.1 --bias_l1=0.1 --regularization_start_epoch=52"
+maxfit_conf="--regularization=max-fit --l1=0.1 --bias_l1=0.5 --regularization_start_epoch=52"
 mse_conf="--use_mse=True --mse_weighted=False"
