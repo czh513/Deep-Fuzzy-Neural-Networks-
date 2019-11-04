192f3d5118f82982c7ae6e2561b6a4a1726a205a
Minh Le <minhle.r7@gmail.com>
2019-11-04 16:29:41 +0100

    fix relog again...

--- models.py
+++ models.py
@@ -33,7 +33,7 @@ class ReLog(nn.Module):
     __constants__ = []
 
     def __init__(self, beta=10):
-        assert(n > 1)
+        assert(beta >= 0)
         super(ReLog, self).__init__()
         self.beta = beta
         self.log_strength = log_strength_start
