fix fan-in calculation, retraining a few models

models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from torch.nn.init import _calculate_fan_in_and_fan_out
 import math
 
 bounds = []
@@ -82,7 +83,7 @@ class Elliptical(nn.Linear):
         linear_term = super(Elliptical, self).forward(input)
         quadratic_term = self._quadratic.forward(input*input)
         a = update_curvature_multiplier(self)
-        fan_in = self.weight.shape[1]
+        fan_in, _ = _calculate_fan_in_and_fan_out(self.weight)
         return -a * quadratic_term + a * math.sqrt(fan_in) + linear_term
 
 class Quadratic(Elliptical):
@@ -103,7 +104,7 @@ class EllipticalCNN(nn.Conv2d):
         linear_term = super(EllipticalCNN, self).forward(input)
         quadratic_term = self._quadratic.forward(input*input)
         a = update_curvature_multiplier(self)
-        fan_in = self.weight.shape[1]
+        fan_in, _ = _calculate_fan_in_and_fan_out(self.weight)
         return -a * quadratic_term + a * math.sqrt(fan_in) + linear_term
 
 class QuadraticCNN(EllipticalCNN):